f46b08f129b195b4fbc70ee7ce4982d5ebb983ac
zliu34-mila <zliu34@inside.artcenter.edu>
2025-10-13 17:38:52 -0700

    update week 4

--- HOMEWORK/week 4/sketch.js
+++ HOMEWORK/week 4/sketch.js
@@ -1,7 +1,35 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let bubbles = [];
 
+function setup() {
+  createCanvas(600, 600);
+  colorMode(HSB, 360, 100, 100);
+  noStroke();
+
+  for (let i = 0; i < 30; i++) {
+    let bubble = {
+      x: random(width),
+      y: random(height),
+      xMove: random(2000),
+      yMove: random(2000),
+      size: random(10, 40),
+      speed: random(0.001, 0.008),
+      hue: random(180, 260)
+    };
+    bubbles.push(bubble);
+  }
+}
+s
 function draw() {
-  background(220);
+  background(220,20,12,0.2); 
+
+  for (let b of bubbles) {
+    b.x = noise(b.xMove) * width;
+    b.y = noise(b.yMove) * height;
+
+    fill(b.hue, 80, 100, 0.6);
+    circle(b.x, b.y, b.size);
+
+    b.xMove += b.speed;
+    b.yMove += b.speed * 0.5;
+  }
 }
