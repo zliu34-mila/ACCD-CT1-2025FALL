7f48f934c31b599872a117ddd8029587e75d929d
zliu34-mila <zliu34@inside.artcenter.edu>
2025-10-13 17:55:24 -0700

    update code

--- HOMEWORK/week 4/sketch.js
+++ HOMEWORK/week 4/sketch.js
@@ -1,9 +1,9 @@
-let bubbles = [];
+let bubbles = []
 
 function setup() {
-  createCanvas(600, 600);
-  colorMode(HSB, 360, 100, 100);
-  noStroke();
+  createCanvas(600, 600)
+  colorMode(HSB, 360, 100, 100)
+  noStroke()
 
   for (let i = 0; i < 30; i++) {
     let bubble = {
@@ -15,21 +15,28 @@ function setup() {
       speed: random(0.001, 0.008),
       hue: random(180, 260)
     };
-    bubbles.push(bubble);
+    bubbles.push(bubble)
   }
 }
-s
+
 function draw() {
-  background(220,20,12,0.2); 
+  background(220,20,12,0.2)
 
   for (let b of bubbles) {
-    b.x = noise(b.xMove) * width;
-    b.y = noise(b.yMove) * height;
+    let noiseX = noise(b.xMove) * width
+    let noiseY = noise(b.yMove) * height
+    let dx = mouseX - noiseX
+    let dy = mouseY - noiseY
+    let disToMouse = dist(mouseX, mouseY, noiseX, noiseY)
+    let attraction = map(disToMouse, 0, width, 0.1, 0)
 
-    fill(b.hue, 80, 100, 0.6);
-    circle(b.x, b.y, b.size);
+    b.x = noiseX + dx * attraction
+    b.y = noiseY + dy * attraction
 
-    b.xMove += b.speed;
-    b.yMove += b.speed * 0.5;
+    fill(b.hue, 80, 100, 0.6)
+    circle(b.x, b.y, b.size)
+
+    b.xMove += b.speed
+    b.yMove += b.speed * 0.5
   }
 }
