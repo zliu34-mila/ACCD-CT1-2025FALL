5c037d5ae8dbbc4075f05a27cdcd55206e5acdc5
zliu34-mila <zliu34@inside.artcenter.edu>
2025-10-20 16:20:36 -0700

    update homework

--- HOMEWORK/week 5/sketch.js
+++ HOMEWORK/week 5/sketch.js
@@ -1,46 +1,46 @@
-let numArcs = 10;
-let initRadius;
-let strWeight = 10;
-let myText = "";
+let numArcs = 10
+let initRadius
+let strWeight = 10
+let myText = ""
 
 function preload() {
 
-  Lines = loadStrings('myText.txt');
+  Lines = loadStrings('myText.txt')
 }
 
 function setup() {
-  createCanvas(800, 800);
-  colorMode(HSB, TWO_PI, 1, 1);
-  initRadius = width * 0.1;
-  textAlign(CENTER, CENTER);
-  textSize(20);
-  noStroke();
-  myText = Lines.join(' ');
+  createCanvas(800, 800)
+  colorMode(HSB, TWO_PI, 1, 1)
+  initRadius = width * 0.1
+  textAlign(CENTER, CENTER)
+  textSize(20)
+  noStroke()
+  myText = Lines.join(' ')
 }
 
 function draw() {
-  background(0);
+  background(0)
 
   for (let i = 0; i < numArcs; i++) {
-    fill(color(i * TWO_PI / numArcs, 0.9, 1));
+    fill(color(i * TWO_PI / numArcs, 0.9, 1))
 
-    push();
-    translate(width / 2, height / 2);
+    push()
+    translate(width / 2, height / 2)
 
-    rotate(sin(millis() * 0.0004 * (i * 0.8 + 0.5) + i * TWO_PI));
+    rotate(sin(millis() * 0.0004 * (i * 0.8 + 0.5) + i * TWO_PI))
 
-    let radius = initRadius + i * strWeight * 4;
-    let indexOffset = (frameCount * 0.15 + i * 10) % myText.length;
+    let radius = initRadius + i * strWeight * 2
+    let indexOffset = (frameCount * 0.15 + i * 10) % myText.length
 
     for (let j = 0; j < 36; j++) {
-      push();
-      rotate((TWO_PI / 36) * j);
-      translate(0, -radius);
-      let charIndex = (indexOffset + j) % myText.length;
-      text(myText.charAt(charIndex), 0, 0);
-      pop();
+      push()
+      rotate((TWO_PI / 36) * j)
+      translate(0, -radius)
+      let charIndex = (indexOffset + j) % myText.length
+      text(myText.charAt(charIndex), 0, 0)
+      pop()
     }
 
-    pop();
+    pop()
   }
 }
