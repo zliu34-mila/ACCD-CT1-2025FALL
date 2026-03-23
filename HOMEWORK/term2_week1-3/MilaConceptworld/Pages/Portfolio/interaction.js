ff318469318719be95782df042b557ad9e492268
zliu34-mila <zliu34@inside.artcenter.edu>
2026-03-23 00:59:35 -0700

    update

--- HOMEWORK/term2_week1-3/MilaConceptworld/Pages/Portfolio/interaction.js
+++ HOMEWORK/term2_week1-3/MilaConceptworld/Pages/Portfolio/interaction.js
@@ -44,8 +44,8 @@ document.head.appendChild(styleTag);
 
 // 👉 把文件路径换成你实际的页面地址
 const tabPageMap = {
-  'about me':  'index.html',
-  'interests': './interests.html',
+  'previous works': './index.html',
+  'processing works': './processing-works.html',
 };
 
 const currentPage = window.location.pathname.split('/').pop() || 'index.html';
@@ -67,7 +67,7 @@ document.querySelectorAll('.tab-btn').forEach(btn => {
 });
 
 const rightBtnMap = {
-  'see my CV':  './images/CV_Mila.pdf',  
+  'see my CV':  '../Aboutme/images/CV_Mila.pdf', 
   'contact':    './contact.html',
 };
 
@@ -78,4 +78,59 @@ document.querySelectorAll('.tab-rightbtn').forEach(btn => {
       window.open(rightBtnMap[tabName], '_blank'); // 新标签页打开
     }
   });
+});
+
+// video
+const videos = [
+    './images/video/spacecraft.mp4',
+    './images/video/Airflow.mp4',
+    './images/video/particles.mp4',
+    './images/video/video4.mp4',
+    './images/video/video5.mp4'
+    
+];
+let currentIndex = 0;
+const videoEl = document.querySelector('.video-container video');
+
+videoEl.addEventListener('ended', () => {
+    currentIndex = (currentIndex + 1) % videos.length;
+    videoEl.src = videos[currentIndex];
+    videoEl.play();
+});
+
+
+const sections = [
+    document.querySelector('.section-video'),
+    document.querySelector('.section-previousworks'),
+    document.querySelector('.section-processingworks')
+];
+
+let current = 0;
+let isAnimating = false;
+
+function goTo(index) {
+    if (index < 0 || index >= sections.length) return;
+    if (isAnimating) return;
+    isAnimating = true;
+
+    // 新section滑上来
+    sections[index].classList.add('active');
+
+    // 旧section如果往回滚，需要退下去
+    if (index < current) {
+        sections[current].classList.remove('active');
+    }
+
+    current = index;
+
+    setTimeout(() => { isAnimating = false; }, 800);
+}
+
+// 监听滚轮
+window.addEventListener('wheel', (e) => {
+    if (e.deltaY > 0) {
+        goTo(current + 1); // 向下
+    } else {
+        goTo(current - 1); // 向上
+    }
 });
